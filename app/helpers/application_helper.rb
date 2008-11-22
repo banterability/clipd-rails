49c326ad1fbf46b84fc30e8b50e17d20beca8e46
Jeffrey Long <jeff@banterability.com>
2008-11-22 01:12:58 -0800

    new layout, timezone support, relative timestamps

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def timeago(time, options = {})
+    options[:class] ||= "timeago"
+    content_tag(:abbr, time.strftime("%b %d, %Y, %I:%M %p"), options.merge(:title => time.strftime("%b %d, %Y, %I:%M %p"))) if time
+  end
+  
 end
