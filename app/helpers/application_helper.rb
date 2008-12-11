3522dfe5356783e96b1a89d5c86e4aeaa7bffeeb
Jeffrey Long <jeff@banterability.com>
2008-12-10 18:10:31 -0800

    made wb_truncate helper to multibyte aware

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,8 +8,8 @@ module ApplicationHelper
   
   def wb_truncate(text, length = 45, truncate_string = "...")
     return if text.nil?
-    l = length - truncate_string.chars.length
-    text.chars.length > length ? text[/\A.{#{l}}\w*\;?/m][/.*[\w\;]/m] + truncate_string : text
+    l = length - truncate_string.mb_chars.length
+    text.mb_chars.length > length ? text[/\A.{#{l}}\w*\;?/m][/.*[\w\;]/m] + truncate_string : text
   end
   
 end
