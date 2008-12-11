0e7234818423c3bfc0f6631d1e4a74121ce8f961
Jeffrey Long <jeff@banterability.com>
2008-12-10 20:43:58 -0800

    Made truncate helper multibyte aware, added footer links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,10 +6,10 @@ module ApplicationHelper
     content_tag(:abbr, time.strftime("%b %d, %Y, %I:%M %p"), options.merge(:title => time.strftime("%b %d, %Y, %I:%M %p"))) if time
   end
   
-  def wb_truncate(text, length = 45, truncate_string = "...")
+  def truncate_at_wb(text, length = 45, truncate_string = "...")
     return if text.nil?
-    l = length - truncate_string.chars.length
-    text.chars.length > length ? text[/\A.{#{l}}\w*\;?/m][/.*[\w\;]/m] + truncate_string : text
+    l = length - truncate_string.mb_chars.length
+    text.mb_chars.length > length ? text[/\A.{#{l}}\w*\;?/m][/.*[\w\;]/m] + truncate_string : text
   end
   
 end
