05fcf5aac79ae08f05c46b066a5b4e0034cc0d9b
Jeffrey Long <jeff@banterability.com>
2009-01-02 22:23:40 -0800

    added GET path for adding clips (temporary)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ ActionController::Routing::Routes.draw do |map|
     
     map.root :controller => "clips"  
     
+    map.connect 'clips/create', :controller => 'clips', :action => 'create'
+    
     map.resources :clips
 
     map.connect ':controller/:action/:id'
